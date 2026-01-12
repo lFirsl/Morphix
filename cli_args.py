87e193cdc2c988015a7b94371ade100c984d0e9c
Florin-Vladimir <florinstancu@protonmail.com>
2026-01-12 20:26:35 +0000

    Moved cli args to it's own standalone file.

---
+++ cli_args.py
@@ -0,0 +1,65 @@
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Compress a video to a target size.")
+    parser.add_argument(
+        "input",
+        nargs="?",
+        help="Path to the input video file.",
+    )
+    parser.add_argument(
+        "--max-mb",
+        type=float,
+        help="Target maximum output size in MB.",
+    )
+    parser.add_argument(
+        "--test",
+        action="store_true",
+        help="Use test defaults for input and max-mb.",
+    )
+    parser.add_argument(
+        "--output",
+        default=".output/output.mp4",
+        help="Path to the output file.",
+    )
+    parser.add_argument(
+        "--disable-logs",
+        dest="disable_logs",
+        action="store_true",
+        default=True,
+        help="Disable ffmpeg logging output.",
+    )
+    parser.add_argument(
+        "--enable-logs",
+        dest="disable_logs",
+        action="store_false",
+        help="Enable ffmpeg logging output.",
+    )
+    parser.add_argument(
+        "--overwrite",
+        dest="overwrite",
+        action="store_true",
+        default=True,
+        help="Overwrite output file if it exists.",
+    )
+    parser.add_argument(
+        "--no-overwrite",
+        dest="overwrite",
+        action="store_false",
+        help="Do not overwrite output file if it exists.",
+    )
+    args = parser.parse_args()
+
+    if args.test:
+        if args.input is None:
+            args.input = ".example_videos/Rainmaker Pre-Pop WipeOut.mp4"
+        if args.max_mb is None:
+            args.max_mb = 15
+    else:
+        if args.input is None:
+            parser.error("the following arguments are required: input (or use --test)")
+        if args.max_mb is None:
+            parser.error("the following arguments are required: --max-mb (or use --test)")
+
+    return args
